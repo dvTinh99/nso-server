c6dcec1203a20ebd9207b1f09ee55ab1f8a11f80
t <tinh.doan@codecomplete.jp>
2024-05-06 08:40:33 +0700

    update global

--- controllers/history.controller.js
+++ controllers/history.controller.js
@@ -5,111 +5,6 @@ import createError from "http-errors";
 // import startTimer, { setStart, setSpinCode, setTimeInSecs } from '../ws.js';
 import fs from "fs";
 
-const wss = new WebSocketServer({
-  port: process.env.WEBSOCKET_PORT || 8080,
-});
-
-const wsConnected = new Set();
-var timeInSecs = 120;
-var ticker;
-var random = getRandomInt(1000000000);
-var start = false;
-
-var get13Record = await HistoryRepo.getResultHistory(13);
-var historyLastNumber = Array.from(get13Record, (x) => x.result);
-
-var xuThisGame = 0;
-var xuPreviousGame = 0;
-if (get13Record.length > 0) {
-  var xuThisGame = get13Record[get13Record.length - 1].xu;
-  var xuPreviousGame = get13Record[get13Record.length - 2].xu;
-}
-
-var timeOneGame = 2 * 60;
-var timeStartRandom = 15;
-var spinCode = null;
-
-function startTimer(secs) {
-  timeInSecs = parseInt(secs);
-  ticker = setInterval(tick, 1000);
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function getRandom(length) {
-  return Math.floor(
-    Math.pow(10, length - 1) + Math.random() * 9 * Math.pow(10, length - 1)
-  );
-}
-
-async function tick() {
-  var secs = timeInSecs;
-  if (secs == timeStartRandom) {
-    xuPreviousGame = xuThisGame;
-    random = getRandom(9);
-  }
-
-  if (secs == 0) {
-    xuThisGame = getRandomInt(99999);
-    let split = String(random).split("");
-    let sumSplitRandom = 0;
-
-    for (let i of split) {
-      sumSplitRandom += parseInt(i);
-    }
-
-    let lastNumber = sumSplitRandom % 10;
-
-    let history = {
-      xu: xuThisGame,
-      result: lastNumber,
-      total_result: sumSplitRandom,
-      sum: random,
-    };
-    if (historyLastNumber.length >= 13) {
-      historyLastNumber.shift();
-      // await HistoryRepo.shift();
-      historyLastNumber.push(lastNumber);
-      // await HistoryRepo.create(history);
-    } else {
-      // await HistoryRepo.create(history);
-      historyLastNumber.push(lastNumber);
-    }
-  }
-  if (secs > 0) {
-    if (start) {
-      timeInSecs--;
-    }
-  } else {
-    clearInterval(ticker);
-    // startTimer(timeOneGame); // 4 minutes in seconds
-  }
-
-  var mins = Math.floor(secs / 60);
-  secs %= 60;
-  var pretty =
-    (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
-
-  wsConnected.forEach((ws) =>
-    ws.send(
-      JSON.stringify([
-        spinCode,
-        pretty,
-        random,
-        historyLastNumber,
-        xuThisGame,
-        xuPreviousGame,
-      ])
-    )
-  );
-}
-wss.on("connection", function (ws) {
-  wsConnected.add(ws);
-
-  ws.on("close", function () {});
-});
 
 export default {
   getAll: async (req, res, next) => {
@@ -131,35 +26,37 @@ export default {
     }
   },
   create: async (req, res, next) => {
-    try {
-      const vxmm = req.body;
-      console.log("create vxmm", vxmm);
+    console.log('create');
+    
+    // try {
+    //   const vxmm = req.body;
+    //   console.log("create vxmm", vxmm);
 
-      let history = {
-        xu: parseInt(vxmm.xuThisGame.replace(/,/g, ""), 10),
-        spin_code: vxmm.spinId,
-        status: 0,
-      };
-      let checkExit = await History.findBySpinCode(vxmm.spinId);
-      if (checkExit.length <= 0) {
-        let newHistory = await History.create(history);
-      }
+    //   let history = {
+    //     xu: parseInt(vxmm.xuThisGame.replace(/,/g, ""), 10),
+    //     spin_code: vxmm.spinId,
+    //     status: 0,
+    //   };
+    //   let checkExit = await History.findBySpinCode(vxmm.spinId);
+    //   if (checkExit.length <= 0) {
+    //     let newHistory = await History.create(history);
+    //   }
 
-      spinCode = vxmm.spinId;
-      startTimer(120);
+    //   spinCode = vxmm.spinId;
+    //   startTimer(120);
 
-      if (newHistory.affectedRows == 1) {
-        res.json({
-          message: "create history game",
-          data: req.body,
-          payload: req.payload,
-        });
-      } else {
-        throw createError.BadRequest();
-      }
-    } catch (error) {
-      next(error);
-    }
+    //   if (newHistory.affectedRows == 1) {
+    //     res.json({
+    //       message: "create history game",
+    //       data: req.body,
+    //       payload: req.payload,
+    //     });
+    //   } else {
+    //     throw createError.BadRequest();
+    //   }
+    // } catch (error) {
+    //   next(error);
+    // }
   },
   delete: async (req, res, next) => {
     try {
@@ -201,55 +98,59 @@ export default {
     }
   },
   start: async (req, res, next) => {
-    try {
-      console.log("start");
-      const vxmm = req.body;
-      let history = {
-        spin_code: vxmm.spinId,
-      };
+    console.log('start');
+    
+    // try {
+    //   console.log("start");
+    //   const vxmm = req.body;
+    //   let history = {
+    //     spin_code: vxmm.spinId,
+    //   };
 
-      let checkExit = await History.findBySpinCode(vxmm.spinId);
-      console.log("checkExit", checkExit.length);
-      if (checkExit.length <= 0) {
-        let rs = await History.create(history);
-      }
-      console.log("start timer", vxmm.time);
-      spinCode = vxmm.spinId;
-      setStart(true);
-      let [minus, second] = vxmm.time.split(":");
-      timeInSecs = (parseInt(minus) * 60 + parseInt(second));
-      res.json({
-        message: "call start history game",
-        data: req.body,
-        payload: req.payload,
-      });
-    } catch (error) {
-      next(error);
-    }
+    //   let checkExit = await History.findBySpinCode(vxmm.spinId);
+    //   console.log("checkExit", checkExit.length);
+    //   if (checkExit.length <= 0) {
+    //     let rs = await History.create(history);
+    //   }
+    //   console.log("start timer", vxmm.time);
+    //   spinCode = vxmm.spinId;
+    //   setStart(true);
+    //   let [minus, second] = vxmm.time.split(":");
+    //   timeInSecs = (parseInt(minus) * 60 + parseInt(second));
+    //   res.json({
+    //     message: "call start history game",
+    //     data: req.body,
+    //     payload: req.payload,
+    //   });
+    // } catch (error) {
+    //   next(error);
+    // }
   },
   end: async (req, res, next) => {
-    try {
-      const vxmm = req.body;
-      let history = {
-        rate: vxmm.rate,
-        xu: parseInt(vxmm.xu.replace(/,/g, ""), 10),
-        number_people: vxmm.people,
-        status: 1,
-      };
-      console.log("end", history);
-      let updateBot = await History.updateBySpinCode(history, vxmm.spinId);
-      setStart(false);
-      if (updateBot.affectedRows == 1) {
-        res.json({
-          message: "call end history game",
-          data: req.body,
-          payload: req.payload,
-        });
-      } else {
-        throw createError.BadRequest();
-      }
-    } catch (error) {
-      next(error);
-    }
+    console.log('end');
+    
+    // try {
+    //   const vxmm = req.body;
+    //   let history = {
+    //     rate: vxmm.rate,
+    //     xu: parseInt(vxmm.xu.replace(/,/g, ""), 10),
+    //     number_people: vxmm.people,
+    //     status: 1,
+    //   };
+    //   console.log("end", history);
+    //   let updateBot = await History.updateBySpinCode(history, vxmm.spinId);
+    //   setStart(false);
+    //   if (updateBot.affectedRows == 1) {
+    //     res.json({
+    //       message: "call end history game",
+    //       data: req.body,
+    //       payload: req.payload,
+    //     });
+    //   } else {
+    //     throw createError.BadRequest();
+    //   }
+    // } catch (error) {
+    //   next(error);
+    // }
   },
 };
--- index.js
+++ index.js
@@ -3,4 +3,128 @@ import WebSocket, { WebSocketServer } from "ws";
 import env from "dotenv";
 env.config();
 
+const wss = new WebSocketServer({
+    port: process.env.WEBSOCKET_PORT || 8080,
+});
+
+const wsConnected = new Set();
+var timeInSecs = 120;
+var ticker;
+var random = getRandomInt(1000000000);
+var start = false;
+
+var get13Record = await HistoryRepo.getResultHistory(13);
+var historyLastNumber = Array.from(get13Record, (x) => x.result);
+
+var xuThisGame = 0;
+var xuPreviousGame = 0;
+if (get13Record.length > 0) {
+    var xuThisGame = get13Record[get13Record.length - 1].xu;
+    var xuPreviousGame = get13Record[get13Record.length - 2].xu;
+}
+
+var timeOneGame = 2 * 60;
+var timeStartRandom = 15;
+var spinCode = null;
+
+function startTimer(secs) {
+    timeInSecs = parseInt(secs);
+    ticker = setInterval(tick, 1000);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRandom(length) {
+    return Math.floor(
+        Math.pow(10, length - 1) + Math.random() * 9 * Math.pow(10, length - 1)
+    );
+}
+
+async function tick() {
+    var secs = timeInSecs;
+    if (secs == timeStartRandom) {
+        xuPreviousGame = xuThisGame;
+        random = getRandom(9);
+    }
+
+    if (secs == 0) {
+        xuThisGame = getRandomInt(99999);
+        let split = String(random).split("");
+        let sumSplitRandom = 0;
+
+        for (let i of split) {
+            sumSplitRandom += parseInt(i);
+        }
+
+        let lastNumber = sumSplitRandom % 10;
+
+        let history = {
+            xu: xuThisGame,
+            result: lastNumber,
+            total_result: sumSplitRandom,
+            sum: random,
+        };
+        if (historyLastNumber.length >= 13) {
+            historyLastNumber.shift();
+            // await HistoryRepo.shift();
+            historyLastNumber.push(lastNumber);
+            // await HistoryRepo.create(history);
+        } else {
+            // await HistoryRepo.create(history);
+            historyLastNumber.push(lastNumber);
+        }
+    }
+    if (secs > 0) {
+        if (start) {
+            timeInSecs--;
+        }
+    } else {
+        clearInterval(ticker);
+        // startTimer(timeOneGame); // 4 minutes in seconds
+    }
+
+    var mins = Math.floor(secs / 60);
+    secs %= 60;
+    var pretty =
+        (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
+
+    wsConnected.forEach((ws) =>
+        ws.send(
+            JSON.stringify([
+                spinCode,
+                pretty,
+                random,
+                historyLastNumber,
+                xuThisGame,
+                xuPreviousGame,
+            ])
+        )
+    );
+}
+
+function initNewSpin() {
+    wsConnected.forEach((ws) =>
+        ws.send(
+            JSON.stringify([
+                spinCode,
+                '02:00',
+                random,
+                historyLastNumber,
+                xuThisGame,
+                xuPreviousGame,
+            ])
+        )
+    );
+}
+wss.on("connection", function (ws) {
+    wsConnected.add(ws);
+    ws.on("close", function () { });
+});
+
+global.startTimer = startTimer;
+global.spinCode = spinCode;
+global.initNewSpin = initNewSpin;
+
 import app from "./api.js";
